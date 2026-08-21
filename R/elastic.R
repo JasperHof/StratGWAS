f5b05190f3a61a233ee0cefdd2091866681edaaa
Jasper Pieter Hof <jasper.hof@qgg.au.dk>
2026-08-21 10:59:38 +0200

    chr

--- R/elastic.R
+++ R/elastic.R
@@ -160,7 +160,7 @@ he_reg_spa <- function (filename, pheno, chunk_size = 256, flank_chunks = 1,
                         outfile, batch_size = 4, n_threads = 1,
                         covariates = NULL, SPA = T, spa_pval_threshold = 0.1,
                         annotation = NULL, annot_names = NULL,
-                        binary = F) {
+                        binary = F, chr = NULL) {
 
   #storage.mode(snp_cat) <- "integer"          # <- force integer
   #snp_cat <- as.matrix(snp_cat)               # ensure it's a matrix, not df
@@ -203,7 +203,8 @@ he_reg_spa <- function (filename, pheno, chunk_size = 256, flank_chunks = 1,
     spa_pval_threshold = spa_pval_threshold,
     annotation = annotation,
     annot_names = annot_names,
-    binary = binary
+    binary      = binary,
+    chr         = chr
   )
 
   # hers$genome_h2 contains the genome estimates per alpha
